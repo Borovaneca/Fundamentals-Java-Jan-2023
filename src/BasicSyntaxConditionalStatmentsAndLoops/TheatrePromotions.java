c718ea131ebe11e41706c9d5a0be30ca12383063
Petyo Veselinov <borovaneca@virgilio.it>
2023-01-09 19:06:26 +0200

    updated

---
+++ src/BasicSyntaxConditionalStatmentsAndLoops/TheatrePromotions.java
@@ -0,0 +1,52 @@
+package BasicSyntaxConditionalStatmentsAndLoops;
+
+import java.util.Scanner;
+
+public class TheatrePromotions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String day = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        if (age < 0 || age > 122){
+            System.out.println("Error!");
+        } else if (age <= 18) {
+            switch (day) {
+                case "Weekday":
+                    System.out.println("12$");
+                    break;
+                case "Weekend":
+                    System.out.println("15$");
+                    break;
+                case "Holiday":
+                    System.out.println("5$");
+                    break;
+            }
+        } else if (age <= 64) {
+            switch (day) {
+                case "Weekday":
+                    System.out.println("18$");
+                    break;
+                case "Weekend":
+                    System.out.println("20$");
+                    break;
+                case "Holiday":
+                    System.out.println("12$");
+                    break;
+            }
+        } else {
+            switch (day) {
+                case "Weekday":
+                    System.out.println("12$");
+                    break;
+                case "Weekend":
+                    System.out.println("15$");
+                    break;
+                case "Holiday":
+                    System.out.println("10$");
+                    break;
+            }
+        }
+    }
+}
