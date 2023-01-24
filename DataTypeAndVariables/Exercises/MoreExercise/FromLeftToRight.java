11a057faf4ce8ec8f8f3a90f492719f3a238e997
Petyo Veselinov <borovaneca@virgilio.it>
2023-01-24 13:53:06 +0200

    Update

--- DataTypeAndVariables/Exercises/MoreExercise/FromLeftToRight.java
+++ DataTypeAndVariables/Exercises/MoreExercise/FromLeftToRight.java
@@ -44,3 +44,36 @@ public class FromLeftToRight {
 
     }
 }
+
+
+
+// Another way to solve the task
+
+
+import java.util.Scanner;
+ 
+public class FromLeftToTheRight_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int inputLines = Integer.parseInt(scanner.nextLine());
+ 
+        for (int i = 0; i < inputLines; i++) {
+            String[] inputArray = scanner.nextLine().split(" ");
+            long firstNumber = Long.parseLong(inputArray[0]);
+            long secondNumber = Long.parseLong(inputArray[1]);
+ 
+            long currentNumber = Math.abs(Math.max(firstNumber, secondNumber));
+            long sumOfDigits = 0;
+            long currentDigit;
+ 
+            while (currentNumber > 0) {
+                currentDigit = (currentNumber % 10);
+                sumOfDigits = sumOfDigits + currentDigit;
+ 
+                currentNumber = currentNumber / 10;
+            }
+            System.out.println(sumOfDigits);
+        }
+    }
+}
